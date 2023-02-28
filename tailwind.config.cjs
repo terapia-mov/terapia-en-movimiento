30a7f78654ef786d1a92e856b3c57d9c0e087eb3
emrosas <emireles.rosas@gmail.com>
2023-02-28 09:58:28 -0600

    Set up main font-family

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     fontFamily: {
-      sans: ["General Sans", "Arial", "sans-serif"],
+      sans: ["GeneralSans-Variable", "Arial", "sans-serif"],
     },
     extend: {},
   },
