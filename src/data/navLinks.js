388f353ccd520dfef138ac932b391e91c350e744
emrosas <emireles.rosas@gmail.com>
2023-02-28 13:51:10 -0600

    Added other pages and made Nav link to navLinks.js

---
+++ src/data/navLinks.js
@@ -0,0 +1,20 @@
+const navLinks = [
+  {
+    name: "Programas",
+    path: "/programas",
+  },
+  {
+    name: "Voluntariado",
+    path: "/voluntariado",
+  },
+  {
+    name: "Blog",
+    path: "/blog",
+  },
+  {
+    name: "Contacto",
+    path: "/contacto",
+  },
+];
+
+export default navLinks;
