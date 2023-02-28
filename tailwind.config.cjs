86c01cfe616db5109436db85938d32bc4313c2b3
emrosas <emireles.rosas@gmail.com>
2023-02-28 11:45:58 -0600

    home here core framing

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,16 @@ module.exports = {
     fontFamily: {
       sans: ["GeneralSans-Variable", "Arial", "sans-serif"],
     },
+    colors: {
+      "m-white": "#FFFFFF",
+      "m-black": "#000000",
+      "m-orange": "#E5A861",
+      "m-yellow": "#F1CB7E",
+      "m-pink": "#F5B9A3",
+      "m-off-white": "#F6F5F3",
+      "m-dark-blue": "#273A54",
+      "m-blue": "#869DC1",
+    },
     extend: {
       spacing: {
         "5%": "5%",
